b93b557c87f1ecaa3e4013ea87e842235e681076
Pawan <rkpawanpatidar12345@gmail.com>
2023-04-23 10:40:39 +0530

    Check Whether a String Starts and Ends With Certain Characters

---
+++ 04-apr.js
@@ -0,0 +1,18 @@
+// Check Whether a String Starts("S") and Ends"G" With Certain Characters
+function checkString(str) {
+
+    // check if the string starts with S and ends with G
+    if(str.startsWith('S') && str.endsWith('G')) {
+        console.log('The string starts with S and ends with G');
+    }
+    else if(str.startsWith('S')) {
+        console.log('The string starts with S but does not end with G');
+    }
+     else if(str.endsWith('G')) {
+        console.log('The string starts does not with S but end with G');
+    }
+    else {
+        console.log('The string does not start with S and does not end with G');
+    }
+}
+checkString("String");
