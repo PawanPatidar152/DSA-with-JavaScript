9a53cf7f15f3d96cb5a5f9e1358efd580fb3e77c
Pawan <rkpawanpatidar12345@gmail.com>
2023-04-23 10:14:22 +0530

    Program to swap two variables value

---
+++ 09-feb.js
@@ -0,0 +1,16 @@
+//Program to Swap Two Variables values
+
+
+let a = 100;
+let b = 69;
+
+//create a temporary variable
+let temp;
+
+//swap variables
+temp = a;
+a = b;
+b = temp;
+
+console.log(`The value of a after swapping: ${a}`);
+console.log(`The value of b after swapping: ${b}`);
