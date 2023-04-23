1ed8b6a7b2e9d83bfdc4e6fdd894012797f4665e
Pawan <rkpawanpatidar12345@gmail.com>
2023-04-23 10:39:30 +0530

    Program to Create Objects in Different Ways

---
+++ 31-mar.js
@@ -0,0 +1,19 @@
+//Program to Create Objects in Different Ways
+
+
+// There are different ways to create new objects:
+
+
+// Create a single object, using an object literal.
+
+const person1 = {firstName:"John", lastName:"Doe", age:50,eyeColor:"blue"};
+
+// Create a single object, with the keyword new.
+
+const person2 = new Object();
+person2.firstName = "John1";
+person2.lastName = "Doe2";
+person2.age = 501;
+person2.eyeColor = "bluee"; 
+
+console.log(person1,person2)
