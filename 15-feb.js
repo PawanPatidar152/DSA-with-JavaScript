1062085bb00acb8ecc2873f934dbbcb92d22e3f5
Pawan <rkpawanpatidar12345@gmail.com>
2023-04-23 10:19:25 +0530

    Program to convert celsius to Fahrenheit

---
+++ 15-feb.js
@@ -0,0 +1,12 @@
+//Program to Convert Celsius to Fahrenheit
+
+
+//Making a function
+function celsiusToFahrenheit(celsius) 
+{
+  //Using formul to direct convert celsius to fahrenheit
+  let Fahrenheit = celsius * 9 / 5 + 32;
+    console.log(celsius,"celsius in Fahrenheit is",Fahrenheit,"fahrenheit");
+}
+celsiusToFahrenheit(35);
+celsiusToFahrenheit(100)
