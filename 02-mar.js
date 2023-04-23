fd48ef2ed34e87897c785290e34374bf437c11c8
Pawan <rkpawanpatidar12345@gmail.com>
2023-04-23 10:27:24 +0530

    Program to Find the Factors of a Number

---
+++ 02-mar.js
@@ -0,0 +1,14 @@
+//Program to Find the Factors of a Number
+
+const num = 100;
+
+console.log(`The factors of ${num} is:`);
+
+// looping through 1 to num
+for(let i = 1; i <= num; i++) {
+
+    // check if number is a factor
+    if(num % i == 0) {
+        console.log(i);
+    }
+}
