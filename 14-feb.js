56300742266620ce1e64594e51e6f7f1ea42e8c8
Pawan <rkpawanpatidar12345@gmail.com>
2023-04-23 10:18:18 +0530

    Program to find the square root of a number

---
+++ 14-feb.js
@@ -0,0 +1,7 @@
+//Program to find the square root of a number
+
+let num = 100
+let squareRoot = Math.sqrt(num);
+
+// displays square root
+console.log("The Square Root of " + num + " is " + squareRoot);
