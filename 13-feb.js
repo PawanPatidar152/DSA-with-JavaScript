6ea9cfd2001a05ae589cd44153d310d61025085b
Pawan <rkpawanpatidar12345@gmail.com>
2023-04-23 10:15:42 +0530

    Program to convert kilometers to Miles

---
+++ 13-feb.js
@@ -0,0 +1,16 @@
+//Program to Convert Kilometers to Miles
+
+let kilometers = 25;
+
+// formula convert kilometers to miles 
+let miles = kilometers * 0.621371
+
+console.log(kilometers,"kilometers",miles,"miles");
+
+//Another Method
+ function kmToMiles(x){
+let y=x*0.621371
+console.log(x,"kilometers",y,"miles");
+ }
+
+ kmToMiles(25)
