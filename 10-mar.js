4b13edcb4d70a21f6f3f2ad6824712a3a25daac6
Pawan <rkpawanpatidar12345@gmail.com>
2023-04-23 10:30:46 +0530

    Program to Find LCM

---
+++ 10-mar.js
@@ -0,0 +1,19 @@
+//Program to Find LCM
+
+// Input must be positive
+const num1 = 12
+const num2 = 2
+
+// higher number among number1 and number2 is stored in min
+if(num1>num2){
+    min=num1
+}
+else min=num2
+// while loop
+while (true) {
+    if (min % num1 == 0 && min % num2 == 0) {
+        console.log(`The LCM of ${num1} and ${num2} is ${min}`);
+        break;
+    }
+    min++;
+}
