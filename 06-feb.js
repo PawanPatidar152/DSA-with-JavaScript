763c768d8c6857878a1a776f86029cbf47e5ec55
Pawan <rkpawanpatidar12345@gmail.com>
2023-04-23 09:54:44 +0530

    Program to Check if a Number is Odd or Even

---
+++ 06-feb.js
@@ -0,0 +1,10 @@
+//Program to Check if a Number is Odd or Even
+
+let x=1023
+if(x%2==0){
+    // displays  even
+    console.log( "Given number is Even.....")
+}
+else
+// displays odd 
+console.log("Given number is Odd.....")
